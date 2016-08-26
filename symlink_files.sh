a50f792c1379b44982a9f89d944aa64b44253273
Mariano Galan <chinomng@gmail.com>
2016-08-25 23:15:05 -0300

    Symlink_files explicit

--- symlink_files.sh
+++ symlink_files.sh
@@ -2,24 +2,30 @@
 
 shopt -s dotglob # shows dots in file globbing
 
-for i in *; do
+for i in .bashrc .shell .config/i3 .gitconfig .gitignore
+do
   rm $HOME/$i 2>/dev/null # remove duplicates
-  
-  if [[ $i == *~ ] | [ $i == README.md ] | [$i == scripts] ] 
-  then
-      echo "Excluding $i"
-  else
-      ln -s `pwd`/$i $HOME/$i # copy new files in
-  fi
+  ln -s `pwd`/$i $HOME/$i # copy new files in
 done
 
-# Clean up after our naieve loop
-rm $HOME/symlink_files.sh
-rm $HOME/crontab
-rm -rf $HOME/.git
+#for i in *; do
+  #rm $HOME/$i 2>/dev/null # remove duplicates
+  
+  #if [( $i == *~ ) | ( $i == README.md ) | ($i == scripts) ] 
+  #then
+  #    echo "Excluding $i"
+  #else
+  #    ln -s `pwd`/$i $HOME/$i # copy new files in
+  #fi
+#done
 
-mkdir $HOME/.virtualenvs
-mkdir $HOME/.pip
-mkdir $HOME/.pip/packages
+# Clean up after our naieve loop
+#rm $HOME/symlink_files.sh
+#rm $HOME/crontab
+#rm -rf $HOME/.git
+
+#mkdir $HOME/.virtualenvs
+#mkdir $HOME/.pip
+#mkdir $HOME/.pip/packages
 
 echo "Symlinking successful."
